88f8d8e3a8ad5c9ad4dbed0ee542a67a9be9815d
Ahmed <samed.skulj08@gmail.com>
2021-03-23 13:43:56 +0100

    Upravljanje POST i GET requestovima

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html", boolean=False)
 
@@ -13,6 +13,23 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email mora biti veci od 4 karaktera", category="error")
+        elif len(firstName) < 2:
+            flash("Ime mora biti veci od 1 karaktera", category="error")
+        elif password1 != password2:
+            flash("Šifre se ne poklapaju", category="error")
+        elif len(password1) < 7:
+            flash("Šifra mora biti veci od 7 karaktera", category="error")
+        else:
+            flash("Kreirano uspješno!", category="success")
+
     return render_template("sign_up.html")
