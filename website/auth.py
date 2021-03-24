dada5aef5a4981ec4194cea63ae41ff5c5338822
Ahmed <samed.skulj08@gmail.com>
2021-03-24 11:17:23 +0100

    Login i Signup metode

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,39 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Ulogovan uspješno!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+
+            else:
+                flash("Netačni podaci, pokušajte ponovo!", category='error')
+        else:
+            flash("Email ne postoji.", category='error')
+
     return render_template("login.html", boolean=False)
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route("/signup", methods=["GET", "POST"])
@@ -20,8 +43,10 @@ def sign_up():
         firstName = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email vec postoji.', category='error')
+        elif len(email) < 4:
             flash("Email mora biti veci od 4 karaktera", category="error")
         elif len(firstName) < 2:
             flash("Ime mora biti veci od 1 karaktera", category="error")
@@ -30,6 +55,12 @@ def sign_up():
         elif len(password1) < 7:
             flash("Šifra mora biti veci od 7 karaktera", category="error")
         else:
+            novi_korisnik = User(email=email, firstName=firstName,
+                                 password=generate_password_hash(password1, method='sha256'))
+            db.session.add(novi_korisnik)
+            db.session.commit()
+            login_user(user, remember=True)
             flash("Kreirano uspješno!", category="success")
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
