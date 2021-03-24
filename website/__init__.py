d08efcd7e3392d69a434d7285e1007329b164467
Ahmed <samed.skulj08@gmail.com>
2021-03-24 10:50:36 +0100

    Inicijalizacija baze podataka

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -11,11 +11,20 @@ def kreiraj_aplikaciju():
     app.config["SECRET_KEY"] = "samedovrepository"
     app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User, Note
+
+    kreiraj_bazu_podataka(app)
     return app
+
+
+def kreiraj_bazu_podataka(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Kreirana baza podataka")
--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.Datetime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
@@ -15,3 +15,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
+    notes = db.relationship("Note")
